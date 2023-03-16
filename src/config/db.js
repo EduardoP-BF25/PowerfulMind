b827c8b51d837db1feccafe60939b87deaba9966
EduardoP-BF25 <105117644+EduardoP-BF25@users.noreply.github.com>
2023-03-16 09:13:26 -0600

    Modificacion Jueves 16  9:15am!

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ dotenv.config({ path : './.env'});
 
 console.log(process.env.DB_PASS);
 
-export const sequelize = new Sequelize("postgresql://postgres:nehTzGTZn5vwGet1pt69@containers-us-west-183.railway.app:6738/railway");
+export const sequelize = new Sequelize("postgresql://postgres:koLpSLVKiGEcYfomMhvv@containers-us-west-172.railway.app:6436/railway");
 
 // export const sequelize = new Sequelize('powerfulmind', 'postgres', process.env.DB_PASS,{
 //     host: 'localhost',
