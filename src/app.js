d2015172d508dcfd0b39d76d1f41d97be9ebd862
EduardoP-BF25 <105117644+EduardoP-BF25@users.noreply.github.com>
2023-03-23 17:15:02 -0600

    Modificacion Jueves 23 5:14pm!

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ dotevn.config({path: './.env'});
 
 const app = express();
 
-app.use('/', express.static('./public'));
+app.use('/', express.static('./PowerfulMindWebAct'));
 app.use(cors())
 app.use(express.json());
 app.use('/usuario', usuarioRouter); 
